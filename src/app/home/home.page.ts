e3a4a3999f3576c53a1c25efd3afd83f9d67a61f
GuiBaioni <guilhermebaioni35@gmail.com>
2024-04-11 15:09:39 -0300

    AppPagamento

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+selector: 'app-home',
+templateUrl: 'home.page.html',
+styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+constructor(private alertController: AlertController) {}
+formaPgto='';
+valor='';
+final=0;
 
-  constructor() {}
-
+calcularVenda(){
+if (this.formaPgto==='pix'){
+this.final=parseFloat(this.valor)-(parseFloat(this.valor)*0.1)
+;
+}
+else if (this.formaPgto==='deb'){
+this.final=parseFloat(this.valor);
+}
+else{
+this.final=parseFloat(this.valor)*1.1
+}
+this.exibirPeso();
+}
+async exibirPeso() {
+const alert = await this.alertController.create({
+header: 'App Vendas',
+message: `Valor final da venda:
+R$${this.final.toFixed(1)}`,
+buttons: ['OK'],
+});
+await alert.present();
+}
 }
